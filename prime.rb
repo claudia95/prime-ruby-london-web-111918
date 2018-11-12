21568ebb66e643549c974e9d0646f9467804811d
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 20:13:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 # Add  code here!
 
-def prime?(integer) 
-  (2..integer).each do |divide_by|
-    if integer % divide_by == 0 
-      return true
-    end
-  end
-      return false 
-end 
+def prime?(number)
+  if number % 2 == 0 
+    return true 
+  else 
+    return false 
+end
